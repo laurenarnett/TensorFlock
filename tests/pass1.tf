d34508ee2ee5dab8f40a423425b0ee37c1a79b76
Joseph Morag <jm4157@columbia.edu>
2018-02-17 14:18:10 -0500

    Wrote one (hopefully) passing test

---
+++ tests/pass1.tf
@@ -0,0 +1,22 @@
+// This is a line comment
+/* This is a block comment
+* HELLOOOOOOOOOOOOOOOOOOOOOO
+* blah blah blah
+* it has some things in it
+*/
+
+myFavoriteInt : Int;
+myFavoriteInt = 3;
+
+myFavoriteDouble : Double;
+myFavoriteDouble = 3.14e12;
+
+myFavoriteBool : Bool;
+myFavoriteBool = True;
+
+aWorseBool : Bool;
+aWorseBool = False;
+
+scope : Double;
+scope = n; { n: Double; n = 4.12; }
+
