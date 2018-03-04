e1c6d029638485a44a84de9b74a90f2486127dde
Joseph Morag <jmorag@users.noreply.github.com>
2018-03-04 17:38:22 -0500

    Update pass5.tf

--- tests/pass5.tf
+++ tests/pass5.tf
@@ -3,7 +3,7 @@
 fac : Int -> Int;
 fac n = if n == 0 then 1 else 
     if n == 1 then 1 
-    else n * fac (n - 1); // parens broken - this will throw a parse error
+    else n * fac (n - 1);
 
 aops: Int;
 aops a b c d e f g = a + fun0 b * fun4 c / f1 d[i,j] ^ e % f - fun2 g;
