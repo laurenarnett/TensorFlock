d9ae6d11c56f10c632b624f646be229fbec62d25
Lauren Arnett <lba2138@columbia.edu>
2018-03-08 18:23:33 -0500

    Add Nat, remove Double, simplify sast
    Our int types are unsigned; rename nat.
    Remove Double; we don't support these.
    Remove saexpr from SAST.

--- src/sast.ml
+++ src/sast.ml
@@ -1,37 +1,25 @@
 open Ast
 
-type saexpr = typ * saexpr_detail
-and saexpr_detail = 
-    SLiteral of int
-  | SId of string
-  | SAop of saexpr * aop * saexpr
-  | SUnop of uop * saexpr
-  | SApp of saexpr * saexpr
-
 type sexpr = typ * sexpr_detail
 and sexpr_detail = 
     SLiteral of int
   | SFliteral of string
   | SBoolLit of bool
-  | STLit of sexpr list
+  | STLit of shape * string list 
   | SId of string
   | SUnop of uop * sexpr
   | SBinop of sexpr * binop * sexpr
   | SApp of sexpr * sexpr
   | SCondExpr of sexpr * sexpr * sexpr
-  | STensorIdx of string * sexpr list
+  | STensorIdx of shape * string * sexpr list
 
-type sfunc_type = {
-  sfname : string;
-  stypes : typ list
-}
-
-type sfunc_def = {
-  sfname : string;
-  sfargs : string list;
-  smain_expr : expr;
-  sscope : (sfunc_type * sfunc_def) list;
+type sfunc = {
+    sfname : string;
+    stype : typ list;
+    sfargs : string list;
+    sfexpr : sexpr;
+    sscope : sfunc list;
 }
 
 type sfunc = sfunc_type * sfunc_def
-type sprogram = sfunc list
+type sprogram = sexpr * sfunc list
