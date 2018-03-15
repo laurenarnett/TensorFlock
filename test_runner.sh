f7b3639322dc0d64565df2944ad15dea71f111c6
Lauren Arnett <lba2138@columbia.edu>
2018-03-15 17:39:07 -0500

    Fix paths in test_runner.sh

--- test_runner.sh
+++ test_runner.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 failures=""
-for f in tests/pass*.tf; do
+for f in ./tests/syntax_tests/pass*.tf; do
     output=$(./toplevel.native -a $f 2>&1)
     if [ $? -eq 0 ]
     then
@@ -18,9 +18,26 @@ for f in tests/pass*.tf; do
     fi
 done
 
-for f in tests/fail*.tf; do
+for f in ./tests/syntax_tests/fail*.tf; do
     output=$(./toplevel.native -a $f 2>&1)
     if [ $? -eq 2 ]
+    then
+        printf "."
+    else
+        printf "F"
+        failures+=$'--------------------\n'
+        failures+=$'Test '
+        failures+="$f"
+        failures+=$' failed.\n'
+        failures+="$output"
+        failures+=$'\n'
+        failures+=$'--------------------\n'
+    fi
+done
+
+for f in ./tests/semant_tests/pass/*.tf; do
+    output=$(./toplevel.native -a $f 2>&1)
+    if [ $? -eq 0 ]
     then
         printf "."
     else
