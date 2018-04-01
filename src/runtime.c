9aadb5619c7b7469f0925b0033c14825e92cd88a
Nick Buonincontri <nbuonin@gmail.com>
2018-04-01 15:12:43 -0400

    Runtime
    This commit implements tensor printing and fleshes out other tensor
    runtime functions. It:
    - Adds a main function to non-exhaustively verify our code
    - Implements tensor printing using some gross nested loops
    - Changes nats to ints where those values are used to control loops.
    This is to fix a bug in taccess where a nat was evaled -1, which
    actually overflows to some big positive int. I changed them in other
    loops for consistency
    - Sets tdelete to decrement n_refs, check if 0 before freeing the struct
    pointers
    - renames functions to begin with all lower case letters

--- src/runtime.c
+++ src/runtime.c
@@ -8,7 +8,7 @@ struct Tensor {
     nat size; //Internal, the actual number of doubles that the tensor is storing
     nat rank;
     nat *shape;
-    //For garbage collecting, 
+    //For garbage collecting,
     //the number of references currently pointing to the tensor
     nat *n_refs;
     double *array;
@@ -16,25 +16,37 @@ struct Tensor {
 
 typedef struct Tensor T;
 
-T *Talloc(nat rank, nat *shape) {
+T *talloc(nat rank, nat *shape) {
     nat i;
     // calculate number of doubles that the tensor will hold
     nat size = 1;
     for (i = 0; i < rank; ++i) {
         size *= shape[i];
     }
-    
+
     // Allocate memory for tensor struct wrapper
     T *tensor = (T*)malloc(2*sizeof(nat) + 2*sizeof(nat *) + sizeof(double *));
     if (tensor == NULL) {
-        printf("Error allocating memory\n");
+        fprintf(stderr, "Error allocating tensor struct\n");
         exit(1);
     }
 
     // Allocate memory for the three pointers that live inside the Tensor
     tensor->shape  = (nat *)malloc(rank * sizeof(nat));
+    if (tensor->shape == NULL) {
+        fprintf(stderr, "Error allocating tensor struct shape\n");
+        exit(1);
+    }
     tensor->n_refs = (nat *)malloc(sizeof(nat));
-    tensor->array  = (double *)malloc(size * sizeof(double));
+    if (tensor->n_refs == NULL) {
+        fprintf(stderr, "Error allocating tensor struct n_refs\n");
+        exit(1);
+    }
+    tensor->array  = (double *)calloc(size, size * sizeof(double));
+    if (tensor->n_refs == NULL) {
+        fprintf(stderr, "Error allocating tensor struct array\n");
+        exit(1);
+    }
 
     // Write the contents of the tensor to the struct
     tensor->size = size;
@@ -47,34 +59,31 @@ T *Talloc(nat rank, nat *shape) {
     // Default to only one reference on creation
     *(tensor->n_refs) = 1;
 
-    // Write zeros to the data array
-    for (i = 0; i < size; ++i) {
-        *(tensor->array) = 0.0;
-    }
-
     return tensor;
 }
 
 
-void Tdelete(T *tensor) {
-    free(tensor->shape);
-    free(tensor->n_refs);
-    free(tensor->array);
-    free(tensor);
+void tdelete(T *tensor) {
+    if (--*tensor->n_refs == 0) {
+        free(tensor->shape);
+        free(tensor->n_refs);
+        free(tensor->array);
+        free(tensor);
+    }
 }
 
-T *Tnew_ref(T *tensor) {
+T *tnew_ref(T *tensor) {
     T *new_pointer = tensor;
-    (new_pointer->n_refs)++;
+    ++*new_pointer->n_refs;
     return new_pointer;
 }
 
-double* access(T *tensor, ...) 
+double* taccess(T *tensor, ...)
 {
     va_list indices;
     va_start(indices, tensor);
-    nat index = 0;
-    nat i, j, prod;
+    int index = 0;
+    int i, j, prod;
 
     // Calculate the index of the underlying 1D array from the user provided
     // indices
@@ -84,6 +93,12 @@ double* access(T *tensor, ...)
             prod *= (tensor->shape)[j];
         }
         index += va_arg(indices, nat) + prod;
+
+        // Bounds check
+        if (index < 0 || index > (tensor->size - 1)) {
+            fprintf(stderr, "Runtime error: tensor index out of bounds\n");
+            exit(1);
+        }
     }
     va_end(indices);
 
@@ -96,8 +111,9 @@ int print_tensor(T *tensor) {
     nat rank = tensor->rank;
     nat *shape = tensor->shape;
 
-    nat *bracket_locations = alloca((rank - 1) * sizeof(nat));
-    nat i, j;
+    int *bracket_locations = alloca((rank - 1) * sizeof(int));
+    int bracket_loc_len = rank;
+    int i, j;
     /*
      * For example, if the shape of a tensor is <2,3,4> we need to print
      * brackets after every 12th double in the backing array and every 4th
@@ -105,9 +121,9 @@ int print_tensor(T *tensor) {
      * would need to do the same if the shape were <2000,3,4>.
      */
     for (i = 1; i < rank; ++i) {
-        nat prod = 1;
+        int prod = 1;
         for (j = i; j < rank; ++j) {
-            prod *= shape[j];            
+            prod *= shape[j];
         }
         bracket_locations[i] = prod;
     }
@@ -116,13 +132,71 @@ int print_tensor(T *tensor) {
         printf("[");
     }
 
-    // WIP print the tensor contents
-    
+    // Print the tensor contents
+    int k, l, m, no_of_brac;
+    for (k = 0; k < tensor->size - 1; k++) {
+        printf("%.2f ", *(tensor->array + k));
+        for (l = 1; l < bracket_loc_len; ++l) {
+            // If the component index mod the bracket location is 0
+            // then print some brackets
+            if ((k + 1) % bracket_locations[l] == 0) {
+                printf("\b");
+                no_of_brac = bracket_loc_len - l;
+                for (m = 0; m < no_of_brac; ++m) {
+                    printf("]");
+                }
+                printf("\n");
+                for (m = 0; m < no_of_brac; ++m) {
+                    printf("[");
+                }
+                break;
+            }
+        }
+    }
+
+    // Print the last component
+    printf("%.2f", *(tensor->array + tensor->size - 1));
 
     // Finally print the closing brackets
     for (i = 0; i < rank; ++i) {
         printf("]");
     }
 
+    printf("\n");
+
     return 0;
+}
+
+int main(int argc, char *argv[]) {
+    printf("TensorFlock runtime test\n");
+    nat s[] = {2, 2, 2};
+    T *someTensor = talloc(3, s);
+
+    double vals[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0};
+    int i;
+    for (i = 0; i < someTensor->size; i++) {
+        *(someTensor->array + i) = vals[i];
+    }
+
+    printf("Now print the tensor\n");
+    print_tensor(someTensor);
+
+    T *theSameTensor = tnew_ref(someTensor);
+    printf("Now print the same tensor from a different ref\n");
+    print_tensor(theSameTensor);
+
+    printf("Now change some values in the tensor\n");
+    double *tptr = taccess(someTensor, 1, 0, 0);
+    *tptr = 42.0;
+    print_tensor(theSameTensor);
+
+    T *somePtr = someTensor;
+    tdelete(someTensor);
+    if (theSameTensor == somePtr) {
+        printf("Good, smart pointer is being smart\n");
+    }
+
+    tdelete(theSameTensor);
+
+    printf("Tensor testing done\n");
 }
