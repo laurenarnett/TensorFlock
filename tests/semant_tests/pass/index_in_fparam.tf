e351955393f73b7242708999de664e2112510994
Joseph Morag <jm4157@columbia.edu>
2018-04-27 00:35:32 +0100

    Change parser to only accept indices on LHS for vars, not funcs

--- tests/semant_tests/pass/index_in_fparam.tf
+++ tests/semant_tests/pass/index_in_fparam.tf
@@ -1,14 +1,7 @@
 main = 42;
-/* This is broken still :(
+
 t : T<n>; // i : [0,n)
 t[i] = cast (i + n); // evaluates to [5. 6. 7. 8. 9.] (if n = 5)
-
-outer : T<n> -> T<m> -> T<n,m>;
-outer u[i] v[j] = u[i] * v[j];
-
-
-f : T<n> -> T<m> -> T<n,m>;
-f u[i] v[j] = outer (2.0 * u[i]) v + 3;
 
 k : Nat;
 k = 3;
@@ -18,4 +11,3 @@ deduce_n = [1. 2. 3. ];
 
 deduce_m : T<m>;
 deduce_m = [1. 2. 3. 4.];
-*/
