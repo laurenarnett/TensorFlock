76ff2d451e4cb00bce4bda9bc858fee818ea1bdc
Joseph Morag <jm4157@columbia.edu>
2018-04-29 20:01:39 -0400

    Fix mutual recursion test typo

--- tests/codegen/pass/mutual_rec.tf
+++ tests/codegen/pass/mutual_rec.tf
@@ -1,7 +1,7 @@
-main = even 4;
+main = even 400;
 
 even : Nat -> Bool;
-even n = if n == 0 then True else even (n-1);
+even n = if n == 0 then True else odd (n-1);
 odd : Nat -> Bool;
-odd n = if n == 0 then False else odd (n-1);
+odd n = if n == 0 then False else even (n-1);
 
