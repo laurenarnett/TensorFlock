a42237fc0d77ad9c5e32fa0da27f923e6041a2b3
Lauren Arnett <lba2138@columbia.edu>
2018-05-07 09:57:00 -0400

    Fix splitting whitespace bug
    On trying to split on space characters, would make empty strings as
    well as strings of our floats. Filters out the empty strings.

--- src/semant.ml
+++ src/semant.ml
@@ -161,7 +161,9 @@ let rec check_expr expression table indices =
             let shape' = List.map int_of_string shape in
             let components = (String.split_on_char ' ' (String.concat " " 
                                                 (List.tl tensor_string))) in
-            (STensor(shape'), STLit((components), shape')), indices
+            let components' = List.filter (fun substring -> String.length
+                                              substring > 0) components in
+            (STensor(shape'), STLit((components'), shape')), indices
     | Id(s) -> (lookup_symb s, SId(s)), indices
     | Unop(Neg, expr) -> begin 
         match type_of expr with 
