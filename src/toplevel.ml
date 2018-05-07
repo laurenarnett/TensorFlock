54c12c71e459d9bb0c36b3a251ae468e7a304d86
Nick Buonincontri <nbuonin@gmail.com>
2018-05-07 13:15:21 -0400

    Incorporate topsort functionality
    This commit modifies toplevel to run topsort of lambda lifting. It also
    updated the expected result of a lift test to reflect updated formatting
    for Sast.string_of_sfunc

--- src/toplevel.ml
+++ src/toplevel.ml
@@ -21,11 +21,12 @@ let () =
   let ast = Parser.program Scanner.token lexbuf in
   match !action with
      Ast -> print_string (Ast.string_of_program ast)
-   | Sast -> print_string (Sast.string_of_sprogram (Semant.check ast |> Lift.rename_sprogram))
-   | Lift -> print_string (Sast.string_of_sprogram (Semant.check ast |> Lift.lift_sprogram))
+   | Sast -> print_string (Sast.string_of_sprogram (Semant.check ast |> Lift.rename_sprogram |> Topsort.make_topsort))
+   | Lift -> print_string (Sast.string_of_sprogram (Semant.check ast |>
+    Lift.lift_sprogram))
    | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate
-   (Semant.check ast |> Lift.lift_sprogram)))
+   (Semant.check ast |> Lift.lift_sprogram |> Topsort.make_topsort)))
    | Compile -> let mdl = Codegen.translate (Semant.check ast |>
-   Lift.lift_sprogram) in
+   Lift.lift_sprogram |> Topsort.make_topsort) in
    Llvm_analysis.assert_valid_module mdl;
    ignore @@ Llvm_bitwriter.write_bitcode_file mdl "output.ll"
