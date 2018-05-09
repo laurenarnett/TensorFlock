831de05ec929878429354e57cc9268c3179b10c0
Joseph Morag <jm4157@columbia.edu>
2018-05-08 23:53:58 -0400

    Fix the transpose

--- src/codegen.ml
+++ src/codegen.ml
@@ -26,14 +26,17 @@ let printf_func = L.declare_function "printf" printf_t the_module
 let print_tensor_t = L.var_arg_function_type nat_t [| L.pointer_type float_t; nat_t |]
 let print_tensor_func = L.declare_function "print_tensor" print_tensor_t the_module
 
-(* If this throws an error, then something is actually problematic and we have
- * a real bug. *)
-let lookup name map = StringMap.find name map
-
-let _dump_env map = StringMap.iter (fun name _value -> print_endline
+let dump_env map = StringMap.iter (fun name _value -> print_endline
     (name ^ " bound to " ^
     (L.string_of_llvalue _value)
     )) map
+
+(* If this throws an error, then something is actually problematic and we have
+ * a real bug. *)
+let lookup name map = match StringMap.find_opt name map with
+  | Some llval -> llval
+  | None -> dump_env map;
+            failwith @@ "Couldn't find " ^ name ^ " in the environment"
 
 let rec codegen_sexpr (typ, detail) map builder = 
   let cond_expr pred cons alt = 
@@ -95,8 +98,8 @@ let rec codegen_sexpr (typ, detail) map builder =
 
   let handle_id s = match L.lookup_function s the_module with
       None -> L.build_load (lookup s map) s builder
-            | Some f -> print_endline "handle_id failure"; 
-              L.build_call f [||] s builder in
+    | Some f -> print_endline "handle_id failure"; 
+                L.build_call f [||] s builder in
 
   match typ with
   | CNat ->
@@ -194,9 +197,7 @@ let rec codegen_sexpr (typ, detail) map builder =
       | CApp(fn, params) -> fn_call fn params builder
       | CTensorIdx(e, idx) -> 
         let the_expr = codegen_sexpr e map builder in
-        let iarray = [| L.const_int nat_t idx |] in
-        let ptr = L.build_in_bounds_gep the_expr iarray "access_array" builder in
-        L.build_load ptr "load_double" builder
+        L.build_extractelement the_expr (L.const_int nat_t idx) "extract" builder
       | _ -> raise (Failure "Internal error: semant failed")
     end
   | CTensor(_size, _shape) ->
@@ -206,7 +207,9 @@ let rec codegen_sexpr (typ, detail) map builder =
           L.const_vector @@ Array.of_list @@ 
           List.map (L.const_float_of_string float_t) contents
       | CId(s) -> handle_id s
-      | _ -> raise (Failure "WIP")
+      | CApp(fn, params) -> fn_call fn params builder
+      | CCondExpr(pred, cons, alt) -> cond_expr pred cons alt
+      | _ -> failwith @@ "Handling expression: "  ^ string_of_cexpr (typ, detail)
     end
 
 (* Use in declaring global vars *)
@@ -274,16 +277,32 @@ let codegen_fn_body env cfunc =
     (* Allocate scope variables:
         * Returns a new env *) 
     let alloc_scope scope env = 
-      let llvals = List.map handle_const scope in
+      let llvals = List.fold_right (fun assign acc ->
+          match assign.index with
+            | None -> handle_const assign :: acc
+            | Some _ -> acc
+      ) scope [] in
       List.fold_left2 (fun acc assign llval -> 
-          alloc_param acc (assign.typ, assign.name) llval) 
-        env scope llvals in
+          match assign.index with
+            | None -> alloc_param acc (assign.typ, assign.name) llval
+            | Some _ -> acc)
+        env (List.filter (fun assign -> assign.index = None) scope) llvals in
     let env'' = alloc_scope cfunc.locals env' in 
 
     (* codegen on variables in scope, adding their values to env'' *) 
-    ignore @@ List.iter (fun assign -> 
-        ignore @@ L.build_store (codegen_sexpr assign.cexpr env'' fn_builder) 
-          (lookup assign.name env'') fn_builder) cfunc.locals;
+    List.iter (fun assign -> 
+        match assign.index with
+          | None -> ignore @@ L.build_store (codegen_sexpr assign.cexpr env'' fn_builder) 
+                    (lookup assign.name env'') fn_builder
+          | Some i -> 
+              let elt = codegen_sexpr assign.cexpr env'' fn_builder in
+              let vec_ptr = lookup assign.name env'' in
+              let vec = L.build_load vec_ptr "" fn_builder in
+              let new_vec = 
+                  L.build_insertelement vec elt (L.const_int nat_t i) "" fn_builder in
+              ignore @@ 
+              L.build_store new_vec vec_ptr fn_builder;
+          ) cfunc.locals;
 
     let ret_val = codegen_sexpr cfunc.cfexpr env'' fn_builder in
     let _ = L.build_ret ret_val fn_builder in 
