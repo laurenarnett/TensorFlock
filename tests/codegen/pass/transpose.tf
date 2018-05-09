8fb2cce5a99173a69b81fc327aa705f9bc6c007f
Joseph Morag <jm4157@columbia.edu>
2018-05-08 22:33:51 -0400

    Add transpose tests

---
+++ tests/codegen/pass/transpose.tf
@@ -0,0 +1,10 @@
+main = transpose mat;
+
+mat : T<n,m>;
+mat = [[1. 2. 3. 4.] [5. 6. 7. 8.]];
+
+transpose : T<n,m> -> T<m,n>;
+transpose mat = mat'; {
+    mat' : T<m,n>; 
+    mat'[j,i] = mat[i,j];
+}
